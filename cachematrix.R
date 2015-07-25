aebf6f14d1c9a902a2623ed7b2a501f3730b4e2f
Marco <mbazeggio@gmail.com>
2015-07-25 17:03:21 -0300

    Exercicio concluído

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programa referente ao curso de R da Coursera, com finalidade de 
+## utilizar a metodologia de escopo
 
-## Write a short comment describing this function
+## Armazena em cache matrix e seu inverso, permitindo sua recuperação 
+## sem novo cálculo
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(n) {
+                x <<- n
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse) 
+        }
 
-}
 
+## Calcula o inverso da matriz retornada pela função makeCacheMatrix. 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+        }
+
+## Como usar
+## source("cachematrix.R")
+## a <- matrix(1:4,2,2)
+## y <- makeCacheMatrix(a)
+## cacheSolve(y)
